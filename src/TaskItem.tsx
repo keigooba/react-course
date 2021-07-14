68b1d9a5d39770240a55f9f558589d1c7b96798a
keigooba <keigo2356@icloud.com>
2021-07-14 11:11:41 +0900

    firebase

---
+++ src/TaskItem.tsx
@@ -0,0 +1,49 @@
+import React, { useState, VFC } from "react";
+import styles from "./TaskItem.module.css";
+import { ListItem, TextField, Grid } from "@material-ui/core";
+import DeleteOutlineOutlinedIcon from "@material-ui/icons/DeleteOutlineOutlined";
+import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
+import { db } from "./firebase";
+
+type Props = {
+  id: string;
+  title: string;
+};
+
+const TaskItem: VFC<Props> = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const editTask = () => {
+    db.collection("tasks").doc(props.id).set({ title: title }, { merge: true });
+  };
+
+  const deleteTask = () => {
+    db.collection("tasks").doc(props.id).delete();
+  };
+
+  return (
+    <ListItem>
+      <h2>{props.title}</h2>
+      <Grid container justify="flex-end">
+        <TextField
+          InputLabelProps={{
+            shrink: true,
+          }}
+          label="Edit task"
+          value={title}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setTitle(e.target.value)
+          }
+        />
+      </Grid>
+      <button className={styles.taskitem__icon} onClick={editTask}>
+        <EditOutlinedIcon />
+      </button>
+      <button className={styles.taskitem__icon} onClick={deleteTask}>
+        <DeleteOutlineOutlinedIcon />
+      </button>
+    </ListItem>
+  );
+};
+
+export default TaskItem;
